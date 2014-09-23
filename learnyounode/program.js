c419483df4203597d12e471e1ddcb007157bfa64
emmack <eckotzer@gmail.com>
2014-09-23 10:31:33 -0400

    assignment 5 finished

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,6 +1,15 @@
-	var fs = require('fs');
-fs.readFile(process.argv[2], function(err, data){
-	if (err) throw err;
-  	console.log(data.toString().split('\n').length -1)
 
+
+var fs = require('fs');
+var path = require('path')
+
+fs.readdir(process.argv[2], function(err, files){
+	if (err) throw err;
+
+	for (var i = 0; i < files.length; i++) {
+      if (path.extname(files[i]) === '.' + process.argv[3])
+        console.log(files[i]);
+};
+	
 });
+ 
